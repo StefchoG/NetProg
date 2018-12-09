5b18e133b7a654c8cbc8cc22f0e638819998e006
Stefan <ste.grozev.tues19@gmail.com>
2018-12-09 17:31:43 +0200

    hw
    hw

---
+++ regex/input.js
@@ -0,0 +1,18 @@
+regExps = {
+"exercise_1": /[A-Z][a-z]+/,
+"exercise_2": /088[1789]\d{6}/,
+"exercise_3": /[^0-1]+/,
+"exercise_4": /.*/,
+"exercise_5": /^(?:[0-9]|(?:[1-9][0-9])|(?:[1-9][0-9][0-9])|(?:[1-1][0-4][0-9][0-9])|(?:1500))$/,
+"exercise_6": /.*/
+};
+cssSelectors = {
+"exercise_1": "css",
+"exercise_2": "css",
+"exercise_3": "css",
+"exercise_4": "css",
+"exercise_5": "css",
+"exercise_6": "css",
+"exercise_7": "css",
+"exercise_8": "css"
+};
