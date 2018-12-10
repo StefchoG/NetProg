448c812ea735f256ac74c7ff7be93d0575555a9a
Stefan <ste.grozev.tues19@gmail.com>
2018-12-10 23:14:28 +0200

    Update input.js
    regex done css now 🥇

--- regex/input.js
+++ regex/input.js
@@ -2,9 +2,9 @@ regExps = {
 "exercise_1": /[A-Z][a-z]+/,
 "exercise_2": /088[1789]\d{6}/,
 "exercise_3": /[^0-1]+/,
-"exercise_4": /.*/,
+"exercise_4": /^[a-zA-Z][\w.+]{2,13}$/,
 "exercise_5": /^(?:[0-9]|(?:[1-9][0-9])|(?:[1-9][0-9][0-9])|(?:[1-1][0-4][0-9][0-9])|(?:1500))$/,
-"exercise_6": /.*/
+"exercise_6": /class=['"](.*)['"]/
 };
 cssSelectors = {
 "exercise_1": "css",
